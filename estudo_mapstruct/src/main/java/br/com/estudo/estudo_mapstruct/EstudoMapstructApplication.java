f130c45b34104d5e4a8edbaa9cb466b555a79504
Adriano Avelino <adriano-st777@hotmail.com>
2022-08-17 22:24:54 -0300

    inicia controle de versão

---
+++ estudo_mapstruct/src/main/java/br/com/estudo/estudo_mapstruct/EstudoMapstructApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.estudo_mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoMapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoMapstructApplication.class, args);
+	}
+
+}
---
+++ estudo_mapstruct/src/test/java/br/com/estudo/estudo_mapstruct/EstudoMapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.estudo_mapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoMapstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
